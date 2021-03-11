455f95d21ccb4098a7928cd768bfd7e06bcaf368
Elizabeth Figura <zfigura@codeweavers.com>
2021-03-11 16:52:55 -0600

    server: Create in-process synchronization objects for mutexes.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5887,6 +5887,7 @@ enum inproc_sync_type
     INPROC_SYNC_AUTO_EVENT,
     INPROC_SYNC_MANUAL_EVENT,
     INPROC_SYNC_SEMAPHORE,
+    INPROC_SYNC_MUTEX,
 };
 
 
@@ -6799,6 +6800,6 @@ union generic_reply
     struct get_linux_sync_device_reply get_linux_sync_device_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 858
+#define SERVER_PROTOCOL_VERSION 859
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
