4d38fe7030307cb89623be407c3e7199a8f68e0b
Ziqing Hui <zhui@codeweavers.com>
2025-01-15 11:30:40 +0800

    mfreadwrite: Use a transform array for writer.
    Our tests show that there will be 2 transforms created for encoding.
    So we use a array here rather than a single encoder transform.

--- dlls/mfreadwrite/writer.c
+++ dlls/mfreadwrite/writer.c
@@ -46,6 +46,12 @@ struct marker_context
     void *user_context;
 };
 
+struct transform
+{
+    IMFTransform *transform;
+    GUID category;
+};
+
 struct pending_item
 {
     struct list entry;
@@ -58,9 +64,9 @@ struct pending_item
 struct stream
 {
     IMFStreamSink *stream_sink;
-    IMFTransform *encoder;
+    struct transform transforms[2];
     MF_SINK_WRITER_STATISTICS stats;
-    struct list queue;
+    struct list queue; /* struct pending_item */
 };
 
 struct sink_writer
@@ -166,6 +172,15 @@ static HRESULT create_marker_context(unsigned int marker_type, void *user_contex
     return S_OK;
 }
 
+static void stream_release_transforms(struct stream *stream)
+{
+    if (stream->transforms[0].transform)
+        IMFTransform_Release(stream->transforms[0].transform);
+    if (stream->transforms[1].transform)
+        IMFTransform_Release(stream->transforms[1].transform);
+    memset(stream->transforms, 0, sizeof(stream->transforms));
+}
+
 static void sink_writer_release_pending_item(struct pending_item *item)
 {
     list_remove(&item->entry);
@@ -233,8 +248,7 @@ static ULONG WINAPI sink_writer_Release(IMFSinkWriter *iface)
 
             if (stream->stream_sink)
                 IMFStreamSink_Release(stream->stream_sink);
-            if (stream->encoder)
-                IMFTransform_Release(stream->encoder);
+            stream_release_transforms(stream);
             sink_writer_drop_pending_items(stream);
         }
         DeleteCriticalSection(&writer->cs);
@@ -308,7 +322,6 @@ static HRESULT WINAPI sink_writer_SetInputMediaType(IMFSinkWriter *iface, DWORD 
         IMFAttributes *parameters)
 {
     FIXME("%p, %lu, %p, %p.\n", iface, index, type, parameters);
-
     return E_NOTIMPL;
 }
 
@@ -357,8 +370,10 @@ static HRESULT WINAPI sink_writer_BeginWriting(IMFSinkWriter *iface)
                 WARN("Failed to subscribe to events for steam %u, hr %#lx.\n", i, hr);
             }
 
-            if (stream->encoder)
-                IMFTransform_ProcessMessage(stream->encoder, MFT_MESSAGE_NOTIFY_BEGIN_STREAMING, 0);
+            if (stream->transforms[0].transform)
+                IMFTransform_ProcessMessage(stream->transforms[0].transform, MFT_MESSAGE_NOTIFY_BEGIN_STREAMING, 0);
+            if (stream->transforms[1].transform)
+                IMFTransform_ProcessMessage(stream->transforms[1].transform, MFT_MESSAGE_NOTIFY_BEGIN_STREAMING, 0);
         }
 
         if (SUCCEEDED(hr))
@@ -466,8 +481,10 @@ static HRESULT sink_writer_flush(struct sink_writer *writer, unsigned int index)
 
     IMFStreamSink_Flush(stream->stream_sink);
 
-    if (stream->encoder)
-        IMFTransform_ProcessMessage(stream->encoder, MFT_MESSAGE_COMMAND_FLUSH, 0);
+    if (stream->transforms[0].transform)
+        IMFTransform_ProcessMessage(stream->transforms[0].transform, MFT_MESSAGE_COMMAND_FLUSH, 0);
+    if (stream->transforms[1].transform)
+        IMFTransform_ProcessMessage(stream->transforms[1].transform, MFT_MESSAGE_COMMAND_FLUSH, 0);
 
     return sink_writer_place_marker(writer, stream, MFSTREAMSINK_MARKER_ENDOFSEGMENT, 0, NULL);
 }
@@ -738,8 +755,11 @@ static HRESULT WINAPI sink_writer_GetServiceForStream(IMFSinkWriter *iface, DWOR
         hr = sink_writer_get_service(writer->sink, service, riid, object);
     else if ((stream = sink_writer_get_stream(writer, index)))
     {
-        if (stream->encoder)
-            hr = sink_writer_get_service(stream->encoder, service, riid, object);
+        if (stream->transforms[1].transform)
+            hr = sink_writer_get_service(stream->transforms[1].transform, service, riid, object);
+        else if (stream->transforms[0].transform)
+            hr = sink_writer_get_service(stream->transforms[0].transform, service, riid, object);
+
         if (FAILED(hr))
             hr = sink_writer_get_service(stream->stream_sink, service, riid, object);
     }
