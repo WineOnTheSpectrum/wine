WineOnTheSpectrum/wine

commit d58e917e2cb36c6ffcb259356c06171d8f6d4dcc

wined3d: Use wined3d_texture_download_from_texture() even if the dst texture map binding is not valid.
If we are writing the entire dst resource—and this is always the case for a
download, since wined3d_texture_download_from_texture() does not support partial
downloads—there is no reason to try to preserve any current map binding the
destination resource may have. We are going to load the resource into its map
binding anyway.

In practice this can happen if the destination's valid location is CLEARED.

With the Vulkan renderer, in that case, we currently load the src texture into
SYSMEM and then perform a CPU copy. This change avoids that.

dlls/wined3d/surface.c
@@ -1519,7 +1519,6 @@ HRESULT texture2d_blt(struct wined3d_texture *dst_texture, unsigned int dst_sub_
         }
     }
     else if (!sub_resource_is_on_cpu(src_texture, src_sub_resource_idx)
-            && (dst_sub_resource->locations & dst_texture->resource.map_binding)
             && !(dst_texture->resource.access & WINED3D_RESOURCE_ACCESS_GPU))
     {
         /* Download */

commit 533885b157ac542405383c77a4b121fc7642d295

wined3d: Explicitly check for BUFFER/SYSMEM before calling wined3d_texture_download_from_texture().
Do not consider cleared textures here; wined3d_texture_download_from_texture()
cannot handle them.

Fixes: d58e917e2cb36c6ffcb259356c06171d8f6d4dcc

dlls/wined3d/surface.c
@@ -1532,7 +1532,7 @@ HRESULT texture2d_blt(struct wined3d_texture *dst_texture, unsigned int dst_sub_
             return WINED3D_OK;
         }
     }
-    else if (!sub_resource_is_on_cpu(src_texture, src_sub_resource_idx)
+    else if (!(src_sub_resource->locations & (WINED3D_LOCATION_BUFFER | WINED3D_LOCATION_SYSMEM))
             && !(dst_texture->resource.access & WINED3D_RESOURCE_ACCESS_GPU))
     {
         /* Download */