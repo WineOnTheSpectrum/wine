WineOnTheSpectrum/wine

commit 7620f21ff98a85249b0b110bcba9953b32763f28

makefiles: Generate the wow64 symlinks from makedep.

tools/makedep.c
@@ -146,6 +146,7 @@ static struct strarray top_install[NB_INSTALL_RULES];
 static const char *root_src_dir;
 static const char *tools_dir;
 static const char *tools_ext;
+static const char *wine64_dir;
 static const char *exe_ext;
 static const char *fontforge;
 static const char *convert;
@@ -240,6 +241,7 @@ static const char *output_makefile_name = "Makefile";
 static const char *input_file_name;
 static const char *output_file_name;
 static const char *temp_file_name;
+static char cwd[PATH_MAX];
 static int compile_commands_mode;
 static int silent_rules;
 static int input_line;
@@ -4142,12 +4144,9 @@ static void output_compile_commands( const char *dest )
     struct compile_command *cmd;
     unsigned int i;
     const char *dir;
-    char buffer[PATH_MAX];
 
     output_file = create_temp_file( dest );
-
-    getcwd( buffer, sizeof(buffer) );
-    dir = escape_cstring( buffer );
+    dir = escape_cstring( cwd );
 
     output( "[\n" );
     LIST_FOR_EACH_ENTRY( cmd, &compile_commands, struct compile_command, entry )
@@ -4355,6 +4354,16 @@ static void output_top_makefile( struct makefile *make )
         output( "wine: %s\n", loader );
         output( "\t%srm -f $@ && %s %s $@\n", cmd_prefix( "LN" ), ln_s, loader );
         strarray_add( &make->all_targets[0], "wine" );
+    }
+
+    if (wine64_dir)
+    {
+        output( "loader-wow64:\n" );
+	output( "\t%srm -f $@ && %s %s/loader $@\n", cmd_prefix( "LN" ), ln_s, wine64_dir );
+        output( "%s/loader-wow64:\n", wine64_dir );
+	output( "\t%srm -f $@ && %s %s/loader $@\n", cmd_prefix( "LN" ), ln_s, cwd );
+        strarray_add( &make->all_targets[0], "loader-wow64" );
+        strarray_add( &make->all_targets[0], strmake( "%s/loader-wow64", wine64_dir ));
     }
 
     for (i = 0; i < subdirs.count; i++) output_sources( submakes[i] );
@@ -4588,6 +4597,7 @@ int main( int argc, char *argv[] )
 
     atexit( cleanup_files );
     init_signals( exit_on_signal );
+    getcwd( cwd, sizeof(cwd) );
 
     for (i = 0; i < HASH_SIZE; i++) list_init( &files[i] );
     for (i = 0; i < HASH_SIZE; i++) list_init( &global_includes[i] );
@@ -4608,6 +4618,7 @@ int main( int argc, char *argv[] )
     root_src_dir       = get_expanded_make_variable( top_makefile, "srcdir" );
     tools_dir          = get_expanded_make_variable( top_makefile, "toolsdir" );
     tools_ext          = get_expanded_make_variable( top_makefile, "toolsext" );
+    wine64_dir         = get_expanded_make_variable( top_makefile, "wine64dir" );
     exe_ext            = get_expanded_make_variable( top_makefile, "EXEEXT" );
     dll_ext[0]         = get_expanded_make_variable( top_makefile, "DLLEXT" );
     fontforge          = get_expanded_make_variable( top_makefile, "FONTFORGE" );

configure
@@ -794,6 +794,7 @@ BISON
 FLEX
 SED_CMD
 RUNTESTFLAGS
+wine64dir
 toolsdir
 x86_64_DISABLED_SUBDIRS
 x86_64_DELAYLOADFLAG
@@ -6600,6 +6601,8 @@ else
     as_fn_error $? "could not find Wine tools in $wine_cv_toolsdir" "$LINENO" 5
 fi
 toolsdir=$wine_cv_toolsdir
+
+wine64dir=$with_wine64
 
 RUNTESTFLAGS="-q -P wine"
 
@@ -23263,18 +23266,8 @@ include/stamp-h.in: configure.ac aclocal.m4
 fi
 
 
-if test -n "$with_wine64"
+if test -z "$with_wine64"
 then
-        rm -f fonts server 2>/dev/null
-    as_fn_append wine_rules "
-all: loader-wow64 $with_wine64/loader-wow64
-loader-wow64:
-	rm -f \$@ && \$(LN_S) $with_wine64/loader \$@
-$with_wine64/loader-wow64:
-	rm -f \$@ && \$(LN_S) $ac_pwd/loader \$@
-clean::
-	rm -f loader-wow64 $with_wine64/loader-wow64"
-else
     TOP_INSTALL_DEV="$TOP_INSTALL_DEV include"
     TOP_INSTALL_LIB="$TOP_INSTALL_LIB \
 fonts \
@@ -24143,6 +24136,7 @@ x86_64_STRIP = $x86_64_STRIP
 x86_64_DELAYLOADFLAG = $x86_64_DELAYLOADFLAG
 x86_64_DISABLED_SUBDIRS = $x86_64_DISABLED_SUBDIRS
 toolsdir = $toolsdir
+wine64dir = $wine64dir
 RUNTESTFLAGS = $RUNTESTFLAGS
 SED_CMD = $SED_CMD
 FLEX = $FLEX

configure.ac
@@ -196,6 +196,7 @@ else
     AC_MSG_ERROR([could not find Wine tools in $wine_cv_toolsdir])
 fi
 AC_SUBST(toolsdir,[$wine_cv_toolsdir])
+AC_SUBST(wine64dir,[$with_wine64])
 AC_SUBST(RUNTESTFLAGS,["-q -P wine"])
 AC_SUBST(SED_CMD,["LC_ALL=C sed -e 's,@bindir@,\${bindir},g' -e 's,@PACKAGE_STRING@,$PACKAGE_STRING,g' -e 's,@PACKAGE_VERSION@,$PACKAGE_VERSION,g'"])
 
@@ -3642,19 +3643,8 @@ fi
 
 dnl Rules for wineloader
 
-if test -n "$with_wine64"
+if test -z "$with_wine64"
 then
-    dnl Get rid of old symlinks
-    rm -f fonts server 2>/dev/null
-    WINE_APPEND_RULE(
-[all: loader-wow64 $with_wine64/loader-wow64
-loader-wow64:
-	rm -f \$[@] && \$(LN_S) $with_wine64/loader \$[@]
-$with_wine64/loader-wow64:
-	rm -f \$[@] && \$(LN_S) $ac_pwd/loader \$[@]
-clean::
-	rm -f loader-wow64 $with_wine64/loader-wow64])
-else
     TOP_INSTALL_DEV="$TOP_INSTALL_DEV include"
     TOP_INSTALL_LIB="$TOP_INSTALL_LIB \
 fonts \

commit 11858b51cb4d3ab64de89a426b1238f4976a652f

makefiles: Handle absolute path in create_dir() due to --with-wine64=(absolute path).
Since commit 7620f21ff98 (makefiles: Generate the wow64 symlinks from
makedep., 2025-02-18), makedep generates a symlink target for
"<wine64_dir>/loader-wow64".  This causes <wine64-dir> to be added as an
"ignore path," ultimately leading to create_file_directories() and then
create_dir().

create_dir() only expects relative paths.  If it's passed an absolute
path, it will recognize the "/" prefix as an empty path component
followed by slash, and will attempt to mkdir(""), which fails.

Fix this by skipping initial slashes in create_dir().

Fixes: 7620f21ff98a85249b0b110bcba9953b32763f28

tools/makedep.c
@@ -2007,12 +2007,13 @@ static void create_dir( const char *dir )
     char *p, *path;
 
     p = path = xstrdup( dir );
-    while ((p = strchr( p, '/' )))
+    for (;;)
     {
+        while (*p == '/') p++;
+        if (!(p = strchr( p, '/' ))) break;
         *p = 0;
         if (mkdir( path, 0755 ) == -1 && errno != EEXIST) fatal_perror( "mkdir %s", path );
         *p++ = '/';
-        while (*p == '/') p++;
     }
     if (mkdir( path, 0755 ) == -1 && errno != EEXIST) fatal_perror( "mkdir %s", path );
     free( path );