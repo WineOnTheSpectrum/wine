7a6fa5bf3ddd66eedd3ab0742f5fa9882d3dc613
Damien Zammit <damien@zamaudio.com>
2026-07-14 17:32:05 +1000

    Rename server to wineserver && symlink wine to tools/wine/wine for tests

--- build.sh
+++ build.sh
@@ -7,3 +7,9 @@ meson compile -C build0
 mkdir build-w64
 meson setup build-w64 --cross-file x86_64-w64-mingw32.txt
 meson compile -C build-w64
+
+cd build-w64
+if [ ! -e "wine" ]; then
+  ln -sf tools/wine/wine wine
+fi
+cd ..
